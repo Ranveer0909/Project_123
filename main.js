e87a1369c8d11a3fb037da0cdcf58bed8f0a2948
Ranveer0909 <84077077+Ranveer0909@users.noreply.github.com>
2022-12-10 11:56:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 function setup()
 {
-    canvas = createCanvas(400,400);
+    canvas = createCanvas(400, 400);
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas.position(560,50);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    background('#FF0000');
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized! ');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
 }
